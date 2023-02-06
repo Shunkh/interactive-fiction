3b75181c5f77254d22af682c507e0a058339b1d5
Jin2oo2 <nojinhiroaki@icloud.com>
2023-02-06 21:59:03 +0000

    Added Desert story

---
+++ story.py
@@ -0,0 +1,23 @@
+print('When the angle of destiny appeared in front of the king to make his way to the world tree. Angle of destiny suggested four paths to the king to choose from. Every path will take them to the world tree. There’s a need to make the right decision which will take the king and his people to the destination in a precise and timely manner. ')
+print('Angle of destiny suggested Four Paths to the king to choose from. Every path will take them to the world tree. ')
+
+path = input('King Timmons VII must choose one of these paths to reach their destination:\nDesert, Iceland, Jungle, or Volcano: ')
+
+if path == 'Desert':
+    print('The kings and his 12 guardian knights could see anything but sands in the desert where temperature can drop to -10C°at midnight and rise to 45C°in noon. They started to walk towards the pinpoint shown on the neo-map. Having walked 3 consecutive days without a rest, they all fell down from their knee at cold night. One of the knights pointed miles away, telling there is light. They could not believe that, but the light was getting clearer as they got close. It was a small village. They stayed in the village a night. Next day, they were exploring the marketplace to find some foods. At the strange stall, holy water was in display. This water is said to equip you great stamina that you can walk all around this desert without fatigue. The braves shared one glass of holy water and got excessive energy.')
+    print()
+    print('They started their travel to the world tree again. With the help of the magical water, the kings and his company passed through the vast desert. ')
+
+#elif path == 'Iceland':
+
+
+#elif path == 'Jungle':
+
+
+#else:
+
+#Conclusion
+print()
+print('King Timmons VII celebrated when they reached the world tree. Their journey through Iceland would forever be remembered as one of the greatest adventures of all time, and a testament to the human spirit\'s ability and teamwork to overcome even the greatest of obstacles. ')
+print()
+print('End')
